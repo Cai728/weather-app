c2b578a5d023936cf833a793b91f6dcb1499d78e
d8a0 <151666577+d8a0@users.noreply.github.com>
2024-01-17 11:59:45 +0000

    weather card design

--- components/Weather.jsx
+++ components/Weather.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
 const Weather = () => {
+
   return (
-    <div>Weather</div>
+    <div className="flex justify-center items-start h-screen mt-24">
+      <div className="w-64 h-64 p-6 border-2 border-gray-300 rounded-lg text-center bg-gray-100 bg-opacity-70">
+        <div className="mb-4">
+          <span className="block text-lg font-bold pt-6">Min: 10°C</span>
+          <span className="block text-lg font-bold">Max: 20°C</span>
+        </div>
+        <div className="italic text-gray-500">Wind Speed: 15 km/h</div>
+        <p>Humidity: 70%</p>
+        <p>Description: Sunny</p>
+      </div>
+
+    </div>
   )
 }
 
